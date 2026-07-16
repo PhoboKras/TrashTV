b1e08b4d74073897d5c35167296e2f30ea263645
Agnes Aurora Marinho da Silva Cavalcanti <vas0037579@aluno.firjansenaisesi.com.br>
2026-07-16 19:32:46 -0300

    1.0

---
+++ TrashTV/src/Components/ComponentCadastro.jsx
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './src/Routes';
+
+export default function ComponentCadastro(){
+    return(
+        <View>
+            
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
