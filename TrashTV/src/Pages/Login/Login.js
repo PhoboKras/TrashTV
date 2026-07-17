f196c708e2b3044213648d60a1bb9c130bfd3751
tivictoroliveira-beep <ti.victoroliveira@gmail.com>
2026-07-16 21:26:02 -0300

    1.0vk

--- TrashTV/src/Pages/Login/Login.js
+++ TrashTV/src/Pages/Login/Login.js
@@ -1,5 +1,6 @@
 import react from 'react';
 import { ScrollView, View, Text, StyleSheet } from 'react-native';
+import ComponentLogin from '../../Components/ComponentLogin'; 
 
 
 export default function ComponentLogin() {
