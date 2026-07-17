a990e087217ec576a24fea7a8e04af933f7a44e2
Agnes Aurora Marinho da Silva Cavalcanti <vas0037579@aluno.firjansenaisesi.com.br>
2026-07-16 21:16:39 -0300

    new

--- TrashTV/src/Components/ComponentCadastro.jsx
+++ TrashTV/src/Components/ComponentCadastro.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-export default function Cadastro() {
+export default function ComponentCadastro() {
   return (
     <View style={styles.container}>
       <View>
