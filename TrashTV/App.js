c2e4d28fc079f61f96cde097f1bb59406875e0ce
Agnes Aurora Marinho da Silva Cavalcanti <vas0037579@aluno.firjansenaisesi.com.br>
2026-07-16 21:08:03 -0300

    new

--- TrashTV/App.js
+++ TrashTV/App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
-import StackRoutes from './src/Routes/StackRoutes';
+import StackRoutes from './src/Routes/stackRoutes';
 
 export default function App() {
   return (
--- TrashTV/src/Pages/Home/Home.js
+++ TrashTV/src/Pages/Home/Home.js
@@ -25,7 +25,4 @@ export default function Home() {
       
     )
 
-
-
-
 }
